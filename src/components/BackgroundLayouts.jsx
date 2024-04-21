4265515a285272aad91aa22fc369fe2b4f7d83d0
unknown <princeik.akowe@gmail.com>
2024-04-21 21:23:19 +0100

    add

--- src/components/BackgroundLayouts.jsx
+++ src/components/BackgroundLayouts.jsx
@@ -1,71 +1,26 @@
-// import { useStateContext } from '../Context';
-// import Clear from '../assets/images/clear.jpeg';
-// import Cloudy from '../assets/images/Cloudy.jpeg';
-// import Fog from '../assets/images/fog.jpeg';
-// import Rainy from '../assets/images/Rainy.jpeg';
-// import Snow from '../assets/images/snow.jpeg';
-// import Stormy from '../assets/images/Stormy.jpeg';
-// import { useEffect, useState } from 'react';
-
-// const conditionToImageMap = {
-//   clear: Clear,
-//   cloud: Cloudy,
-//   rain: Rainy,
-//   shower: Rainy,
-//   snow: Snow,
-//   fog: Fog,
-//   thunder: Stormy,
-//   storm: Stormy,
-// };
-
-// const BackgroundLayouts = () => {
-//   const { weather } = useStateContext();
-//   const [image, setImage] = useState(Clear);
-
-//   useEffect(() => {
-//     if (weather && weather.conditions) {
-//       const imageString = weather.conditions.toLowerCase();
-//       const conditionKeys = Object.keys(conditionToImageMap);
-//       for (const conditionKey of conditionKeys) {
-//         if (imageString.includes(conditionKey)) {
-//           setImage(conditionToImageMap[conditionKey]);
-//           return; // Exit loop early once a matching condition is found
-//         }
-//       }
-//       // If no matching condition is found, set a fallback image
-//       setImage(Clear);
-//     }
-//   }, [weather]);
-
-//   return <img src={image} alt='weather_image' className='h-screen w-full fixed left-0 top-0 -z-[10]' />;
-// };
-
-// export default BackgroundLayouts;
-
 import { useStateContext } from '../Context';
+import Clear from '../assets/images/clear.jpeg';
+import Cloudy from '../assets/images/Cloudy.jpeg';
+import Fog from '../assets/images/fog.jpeg';
+import Rainy from '../assets/images/Rainy.jpeg';
+import Snow from '../assets/images/snow.jpeg';
+import Stormy from '../assets/images/Stormy.jpeg';
 import { useEffect, useState } from 'react';
 
-import clearImage from '../assets/images/clear.jpeg';
-import cloudyImage from '../assets/images/Cloudy.jpeg';
-import fogImage from '../assets/images/fog.jpeg';
-import rainyImage from '../assets/images/Rainy.jpeg';
-import snowImage from '../assets/images/snow.jpeg';
-import stormyImage from '../assets/images/Stormy.jpeg';
-
 const conditionToImageMap = {
-  clear: clearImage,
-  cloud: cloudyImage,
-  rain: rainyImage,
-  shower: rainyImage,
-  snow: snowImage,
-  fog: fogImage,
-  thunder: stormyImage,
-  storm: stormyImage,
+  clear: Clear,
+  cloud: Cloudy,
+  rain: Rainy,
+  shower: Rainy,
+  snow: Snow,
+  fog: Fog,
+  thunder: Stormy,
+  storm: Stormy,
 };
 
 const BackgroundLayouts = () => {
   const { weather } = useStateContext();
-  const [image, setImage] = useState(clearImage);
+  const [image, setImage] = useState(Clear);
 
   useEffect(() => {
     if (weather && weather.conditions) {
@@ -74,11 +29,10 @@ const BackgroundLayouts = () => {
       for (const conditionKey of conditionKeys) {
         if (imageString.includes(conditionKey)) {
           setImage(conditionToImageMap[conditionKey]);
-          return; // Exit loop early once a matching condition is found
+          return;
         }
       }
-      // If no matching condition is found, set a fallback image
-      setImage(clearImage);
+      setImage(Clear);
     }
   }, [weather]);
 
@@ -86,3 +40,4 @@ const BackgroundLayouts = () => {
 };
 
 export default BackgroundLayouts;
+
